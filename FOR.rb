8e3520c2e93b347e2d7b57141e3457d360836702
Analarie <119441340+Analarie@users.noreply.github.com>
2023-05-22 13:03:04 -0300

    Update and rename SISTEMAS DE REPETIÇÃO to FOR.rb

---
+++ FOR.rb
@@ -0,0 +1,30 @@
+#FOR EM RUBY
+# Exemplo 1: Imprimir os números de 1 a 5 usando um loop 'for'
+for num in 1..5
+  
+    # A variável 'num' representa o número atual do loop.
+    puts num
+  end
+  5
+  # Exemplo 2: Calcular a soma dos números de 1 a 10 usando um loop 'for'
+  sum = 0
+  for num in 1..10
+    # Loop 'for' que itera sobre os números de 1 a 10.
+  
+    # A variável 'num' representa o número atual do loop.
+    # Em cada iteração, o número é adicionado à variável 'sum' para calcular a soma.
+    sum += num
+  end
+  
+  # Imprime a soma dos números de 1 a 10.
+  puts "A soma dos números de 1 a 10 é #{sum}."
+  
+  # Exemplo 3: Iterar sobre um array e imprimir cada elemento usando um loop 'for'
+  fruits = ["maçã", "banana", "laranja"]
+  for fruit in fruits
+    # Loop 'for' que itera sobre cada elemento do array 'fruits'.
+  
+    # A variável 'fruit' representa o elemento atual do loop.
+    # Em cada iteração, o elemento é impresso na tela.
+    puts fruit
+  end
